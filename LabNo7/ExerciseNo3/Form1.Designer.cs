bb07a14dee6a895735be38a2bbdd8973fd5de2bf
Amna270 <233581@students.au.edu.pk>
2024-11-13 21:59:39 +0500

    Add files via upload

---
+++ LabNo7/ExerciseNo3/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ExerciseNo3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Items.AddRange(new object[] { "Numbers\t\tSquare" });
+            listBox1.Location = new Point(39, 33);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(341, 244);
+            listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(109, 292);
+            button1.Name = "button1";
+            button1.Size = new Size(193, 60);
+            button1.TabIndex = 1;
+            button1.Text = "Calculate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Info;
+            ClientSize = new Size(426, 385);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Calculates the Square of First 10 Numbers";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
