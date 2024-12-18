65a89490d6e2b0b22a3bb3d3c6fe2d0401065a60
Amna270 <233581@students.au.edu.pk>
2024-12-18 13:42:30 +0500

    Add files via upload

---
+++ LabNo12/ActivityNo1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Media.Animation;
+
+namespace lab_13_233533
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); 
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime currentTime = DateTime.Now; 
+            UpdateTimeDisplay(currentTime); 
+            Vibrate(); 
+        }
+
+        private void UpdateTimeDisplay(DateTime time)
+        {
+            TimeDisplay.Text = time.ToString("hh:mm:ss tt"); 
+        }
+
+        private void Vibrate()
+        {
+            
+            var animation = new DoubleAnimation
+            {
+                From = 0,
+                To = 5, 
+                Duration = TimeSpan.FromMilliseconds(100), 
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(5) 
+            };
+
+           
+            this.BeginAnimation(Window.LeftProperty, animation);
+        }
+    }
+}
