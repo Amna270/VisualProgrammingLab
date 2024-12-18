e07869c3057458357c14693836fe84dadd0b20ab
Amna270 <233581@students.au.edu.pk>
2024-12-18 13:58:58 +0500

    Add files via upload

---
+++ LabNo12/Task_1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace OrderApp
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> addresses = new List<string>
+        {
+            "123 Main St, City A",
+            "456 Elm St, City B",
+            "789 Oak St, City C"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddressComboBox.ItemsSource = addresses;
+        }
+
+        private void ChangeAddress_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedAddress = AddressComboBox.SelectedItem as string;
+            if (selectedAddress != null)
+            {
+                MessageBoxResult result = MessageBox.Show("Do you want to change the selected address?", "Change Address", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    // Logic to change the address or add a new one
+                    ResultTextBlock.Text = "Address changed successfully!";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an address.");
+            }
+        }
+    }
+}
